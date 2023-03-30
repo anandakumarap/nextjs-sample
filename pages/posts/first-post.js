718d0f98da956588458848a699de35a4461d8996
AnandaKumar A <anandakumar.a@horizontalintegration.com>
2023-03-29 20:53:08 -0500

    nextjs geo

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { NextRequest } from 'next/server';
+
 
 export default function FirstPost() {
     return (
@@ -7,6 +9,25 @@ export default function FirstPost() {
         <h2>
           <Link href="/">Back to home</Link>
         </h2>
+        <div>
+          <span>city: </span>
+          <span>{NextRequest.geo}</span>
+          <span>{NextRequest.geo}</span>
+          
+          {/* <dl>
+            <dt>City:      
+          </dt>
+            <dd>{NextRequest.heades.get('x-vercel-ip-city')}</dd>
+          </dl>
+          <dt>Region:      
+          </dt>
+            <dd>{NextRequest.heades.get('x-vercel-ip-country-region')}</dd>
+          
+          <dt>Country:      
+          </dt>
+            <dd>{NextRequest.heades.get('x-vercel-ip-country')}</dd> */}
+          
+        </div>
       </>
     );
   }

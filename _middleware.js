a8f125a44aa4ca651e27b6c315d06435f50827bb
AnandaKumar A <anandakumar.a@horizontalintegration.com>
2023-03-29 20:41:01 -0500

    imported nextrequest

--- _middleware.js
+++ _middleware.js
@@ -1,7 +1,9 @@
-export  default function middleware(req){
-    console.log(geo.req.city);
-    console.log(geo.req.region);
-    console.log(geo.req.contry);
+import {NextRequest} from 'next/server'
+
+export async function middleware(req: NextRequest) {
+    console.log(geo.req.city)
+    console.log(geo.req.region)
+    console.log(geo.req.contry)
     console.log(req.heades.get('x-vercel-ip-city'))
     console.log(req.heades.get('x-vercel-ip-country-region'))
     console.log(req.heades.get('x-vercel-ip-country'))

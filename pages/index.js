23976bc7b7e54b1f50ceeb7e8dc18fe093359944
AnandaKumar A <anandakumar.a@horizontalintegration.com>
2023-03-29 20:52:11 -0500

    test geo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
-
 export default function Home() {
   return (
     <div className={styles.container}>
